af6c13e0fca0d890ef3424df54f7a778dec411bf
soultomind <soultomind930@gmail.com>
2023-01-15 13:23:01 +0900

    단순 - 작업 내역 커밋

--- README.md
+++ README.md
@@ -55,14 +55,32 @@ src/main/webapp/WEB-INF/web.xml
 
 ### Basic1
 
-##### 1. RequestMapping 애너테이션   
+##### 1. @Controller   
+```
+@Controller
+public class RequestMappingController {
+
+}
+```
+
+##### 2. @RequestMapping 
 
 RequestMappingController.java
 ```
 @RequestMapping(value = {}, method = { RequestMethod.GET})
-```
+public void request(HttpServletRequest request, HttpServletResponse response) {
 
-RequestMapping 애너테이션 기능이 개선된 애너테이션은 아래와 같습니다.
+}
+```
+value = 매핑주소   
+method = 요청 방법
+
+SPRING에서는 내부적으로 매핑주소(value)를 request 메서드로 매핑   
+##### org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping   
+
+
+
+RequestMapping 어노테이션 기능이 개선된 애너테이션은 아래와 같습니다.
 
 + @DeleteMapping
 + @GetMapping
@@ -70,7 +88,19 @@ RequestMapping 애너테이션 기능이 개선된 애너테이션은 아래와 
 + @PostMapping
 + @PutMapping
 
-SPRING 4.3 버전부터 사용이 가능합니다. 
+SPRING 4.3 버전부터 사용이 가능하며 아래와 같이 사용할 수 있습니다.
+```
+@Controller
+@RequestMapping("/request)
+public class RequestMappingController {
+
+	@GetMapping("/process1")
+	public void process1() {
+
+	}
+}
+```
+process1 메서드에서는 /request/process1 의 Url 과 매핑됩니다.
 
 </br>
 </br>
--- basic1/src/main/java/com/springbasic/basic1/RequestMappingController.java
+++ basic1/src/main/java/com/springbasic/basic1/RequestMappingController.java
@@ -9,12 +9,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-//컨트롤러로 사용하기 위한 SPRING 애너테이션
+//컨트롤러로 사용하기 위한 SPRING 어노테이션
 @Controller
 public class RequestMappingController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(RequestMappingController.class);
-	
 	
 	/***
 	 * 
@@ -27,8 +26,15 @@ public class RequestMappingController {
 	 * value = 매핑 주소
 	 * method = 요청 방법
 	 * 
-	 * 현재 @RequestMapping 애너테이션에 선언된 정보라면 매핑주소는 아래와 같으면 GET 요청으로만 호출이 가능합니다.
+	 * 현재 @RequestMapping 선언된 정보라면 매핑주소는 아래와 같으면 GET 요청으로만 호출이 가능합니다.
 	 * 매핑 주소 : http://localhost:8080/basic1/request
+	 * 
+	 * 서버주소 생략
+	 * 현재 해당 프로젝트에서는 /basic1/request로 요청이 되면 해당 메서드와 연결된다. (/ContextRoot + 매핑주소)
+	 * 연결되는 메서드의 이름은 중요하지 않습니다.
+	 *  
+	 * SPRING에서는 내부적으로 매핑주소(value)를 아래의 request 메서드로 매핑을 시킵니다.
+	 * org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping
 	 */
 	public String request(HttpServletRequest request, HttpServletResponse response) {
 		
@@ -42,6 +48,8 @@ public class RequestMappingController {
 		 * </beans:bean>
 		 * 
 		 * 이다. 즉 prefix + 뷰 파일명 + suffix 값을 조합하여 뷰 파일을 찾는다.
+		 * 
+		 * 해당 메서드의 리턴 타입이 없을때(즉 void 일 경우) /request 에 해당하는 뷰 이름을 찾는다 (WEB-INF/views/request.jsp)
 		 */
 		
 		return "requestMapping";
